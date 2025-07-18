bb2f8214a3e983693309ff7f05201c7253fd5688
ShrishailPatil <apple@Shrishails-MacBook-Air.local>
2025-07-18 23:04:49 +0530

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import AuthSessionProvider from "..//components/session-provider"
 import "./globals.css"
-
+import { Analytics } from "@vercel/analytics/next"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <AuthSessionProvider>
           {children}
         </AuthSessionProvider>
